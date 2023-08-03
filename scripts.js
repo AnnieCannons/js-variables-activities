7f7222b492f4481c4a760de0ac9f9ad0fe6c0726
Taylor <taylorlange95@gmail.com>
2023-08-03 14:57:37 -0500

    Completed exercises 1-4.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
 /************************ Exercise 1 - Identify Variables ************************/
 
 // Declare Variables Here
+let totalSales;
+let totalExpense;
+let totalProfit;
+let ageGroup;
+let customerLocation;
+let name;
+let status;
+let spent;
+let salesReturn;
+let costConversion;
+let costClick;
+let ctr;
+let action;
 
 
 
@@ -13,6 +26,12 @@
 /************************ Exercise 2 - Declaring Variables ************************/
 
 // Declare three variables named firstName, lastName, and age. Assign your own values to each variable. 
+let firstName = "Taylor";
+console.log(firstName);
+let lastName = "Lange";
+console.log(lastName);
+let age = "28";
+console.log(age)
 
 
 // Log the variables' values to the console.
@@ -25,8 +44,19 @@
 
 // Create two variables, a and b, and assign them any values.
 
+let a = "apples";
+console.log(a)
+let b = "bananas";
+console.log(b)
+
 // Swap the values of a and b. You may use a third variable to hold a value so you don't overwrite it. Use reassignment—don't change the previous lines of code!
 
+a = b;
+console.log(a);
+let temp = b;
+console.log(temp);
+b = "apples";
+console.log(b);
 // Log the values of a and b before and after the swap.
 
 
@@ -37,6 +67,12 @@
 
 // Declare a constant variable named PI and set its value to 3.14159. 
 
+const PI = "3.14159";
+
 // Then, attempt to reassign a new value to PI. 
 
+PI = "5";
+
 // Observe the error in the console and write a comment here explaining why you can't reassign a constant.
+
+// Constant variables cannot be reassigned values, because "const" is a fixed declaration. In order to reassign a value to a variable, you must use "let".
