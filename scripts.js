6b0e749270f1d0175773ef5ce2878c70aacd8da7
si <scarllett@anniecannons.com>
2023-08-03 13:59:29 -0700

    “final”

--- scripts.js
+++ scripts.js
@@ -2,33 +2,43 @@
 
 // Declare Variables Here
 
-
-
-
-
-
-
-
+let login 
+let totalSales
+let totalExpenses 
+let totalProfit
+let saleBreakdown
+let customer
 
 /************************ Exercise 2 - Declaring Variables ************************/
 
 // Declare three variables named firstName, lastName, and age. Assign your own values to each variable. 
 
+let firstName = "Scarllette";
+let lastName = "IVY";
+let age = "41";
 
 // Log the variables' values to the console.
-
-
-
-
+ 
+console.log ("firstName");
+console.log ("lastName");
+console.log ("age");
 
 /************************ Exercise 3 - Swapping Variables  ************************/
 
 // Create two variables, a and b, and assign them any values.
 
+let a = "colin";
+let b = "catie";
+
 // Swap the values of a and b. You may use a third variable to hold a value so you don't overwrite it. Use reassignment—don't change the previous lines of code!
+
+a = b;
+let c = b;
+b = c;
 
 // Log the values of a and b before and after the swap.
 
+console. log ("")
 
 
 
