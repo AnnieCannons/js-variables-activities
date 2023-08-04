0090f7120f36a9f9e4acacebdee875101136868d
[Hailey G] <[haileydgrace@gmail.com]>
2023-08-04 10:39:57 -0400

    assignment

--- scripts.js
+++ scripts.js
@@ -2,6 +2,31 @@
 
 // Declare Variables Here
 
+let AdSpent; 
+let Employes;
+let Apps; 
+let Upsell;
+let Bundels;
+let Abandanctcart;
+let AgeGroup;
+let CustomerDemograph;
+let Location; 
+let Overview;
+let TotalProfit;
+let TotalExpense;
+let TotalSales; 
+let Name;
+let Status; 
+let Spent; 
+let Return; 
+let CostConversaion; 
+let CostClick;
+let CTR; 
+let Actions; 
+let firstName
+let lastName
+let age
+let searchheresomething
 
 
 
@@ -15,9 +40,13 @@
 // Declare three variables named firstName, lastName, and age. Assign your own values to each variable. 
 
 
+firstName = "Hailey"
+lastName = "Gerhard"
+age = "26"
+
 // Log the variables' values to the console.
 
-
+console.log("Logging information");
 
 
 
@@ -30,13 +59,28 @@
 // Log the values of a and b before and after the swap.
 
 
+let a = 3;
+let b = 4;
 
+console.log("a + b");
 
+let c = 5;
+a = 4;
+b = 3;
 
+console.log(" a + b + c");
 /************************ Exercise 4 - Working with Constants ************************/
 
 // Declare a constant variable named PI and set its value to 3.14159. 
 
+const PI = 3.14159;
+
 // Then, attempt to reassign a new value to PI. 
 
+// PI = 3.123; //
+
 // Observe the error in the console and write a comment here explaining why you can't reassign a constant.
+ // comment: the value has already been assigned/declared for PI meaning I can't reassign the value going forward //
+
+
+
