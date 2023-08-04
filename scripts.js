0cc8933746db4368a0783581b31287477fd0383f
si <scarllett@anniecannons.com>
2023-08-03 17:21:06 -0700

    updated final assig

--- scripts.js
+++ scripts.js
@@ -2,18 +2,31 @@
 
 // Declare Variables Here
 
+let search 
 let login 
-let totalSales
+let totalSales 
 let totalExpenses 
-let totalProfit
-let saleBreakdown
-let customer
+let totalProfit 
+let saleBreakdown // ??
+let adSpent 
+let employes 
+let apps
+let upsell
+let bundels
+let abandanctCart
+let customerDemograph // ??
+let ageGroup
+// Question how to do ages???
+let location
+let boston
+let newYork
+let california
 
 /************************ Exercise 2 - Declaring Variables ************************/
 
 // Declare three variables named firstName, lastName, and age. Assign your own values to each variable. 
 
-let firstName = "Scarllette";
+let firstName = "scarllette";
 let lastName = "IVY";
 let age = "41";
 
@@ -35,18 +48,22 @@ let b = "catie";
 a = b;
 let c = b;
 b = c;
+c = // ??
 
 // Log the values of a and b before and after the swap.
 
-a = colin
-b = catie 
-console. log ("colin"); // ("catie");
-console. log ("catie"); // 
+a = "colin";
+b = "catie";
+console.log ('lets Swap! ' + a + ', use to be colin and catie is now ' + b +'.' )
+//catie use to be colin and catie is now colin
 
 /************************ Exercise 4 - Working with Constants ************************/
 
-// Declare a constant variable named PI and set its value to 3.14159. 
-const = PI
+// Declare a constant variable named PI and set its value to 3.14159.
+
+const = PI;
+PI = "3.14159";
+PI = "4.5";
 // Then, attempt to reassign a new value to PI. 
 
 // Observe the error in the console and write a comment here explaining why you can't reassign a constant.
