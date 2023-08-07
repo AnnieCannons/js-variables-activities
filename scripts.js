fec51816a770a985ec079c7b7f9c3766c451b663
Dorcus Nagadya <dorcusnagadya889@gmail.com>
2023-08-07 13:54:58 -0700

    new js variables.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 /************************ Exercise 1 - Identify Variables ************************/
 
 // Declare Variables Here
+let totalSales;
+let totalProfit;
+let totalExpense;
+let Employes;
+let AgeGroup;
+let Bundels;
+let locations;
+let status = "poused";
+let spent = 50;
+let costConversation = 12.54;
 
 
 
@@ -14,9 +24,13 @@
 
 // Declare three variables named firstName, lastName, and age. Assign your own values to each variable. 
 
+let firstName = 'Dorcus';
+let lastName = 'Nagadya'
+let age = '27';
 
 // Log the variables' values to the console.
 
+console.log("login information")
 
 
 
@@ -24,19 +38,25 @@
 /************************ Exercise 3 - Swapping Variables  ************************/
 
 // Create two variables, a and b, and assign them any values.
+let a = 7;
+let b = 5;
+ console.log('a+b');
 
 // Swap the values of a and b. You may use a third variable to hold a value so you don't overwrite it. Use reassignment—don't change the previous lines of code!
-
+let d = 8;
+ a = 2;
+ b = 6;
 // Log the values of a and b before and after the swap.
 
 
-
+console.log('a + b + c');
 
 
 /************************ Exercise 4 - Working with Constants ************************/
 
 // Declare a constant variable named PI and set its value to 3.14159. 
-
+const PI = 3.14159
 // Then, attempt to reassign a new value to PI. 
-
+Let PI = 4.14159
+const = 4.141559.
 // Observe the error in the console and write a comment here explaining why you can't reassign a constant.
