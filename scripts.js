79504bedb3faddabde17542363d92a3d92a75f06
Dorcus Nagadya <dorcusnagadya889@gmail.com>
2023-10-03 13:49:14 -0700

    finished

--- scripts.js
+++ scripts.js
@@ -8,9 +8,10 @@ let Employes;
 let AgeGroup;
 let Bundels;
 let locations;
-let status = "posed";
-let spent = 50;
-let costConversation = 12.54;
+let status;
+let spent;
+let costConversation;
+
 
 
 
@@ -30,7 +31,7 @@ let age = '27';
 
 // Log the variables' values to the console.
 
-console.log("login information")
+console.log(firstName,lastName, age);
 
 
 
@@ -40,24 +41,27 @@ console.log("login information")
 // Create two variables, a and b, and assign them any values.
 let a = 7;
 let b = 5;
- console.log('a+b');
+ console.log(a,b);
 
 // Swap the values of a and b. You may use a third variable to hold a value so you don't overwrite it. Use reassignment—don't change the previous lines of code!
-let d = 8;
- a = 2;
- b = 6;
+
+a = b;
+console.log(a);
+let temp = 7;
+console.log(temp);
+b = temp
+console.log(a);
 // Log the values of a and b before and after the swap.
 
 
-console.log('a + b + c');
+console.log(a,b);
 
 
 /************************ Exercise 4 - Working with Constants ************************/
 
 // Declare a constant variable named PI and set its value to 3.14159. 
-const pI = 3.14159
+const pi = 3.14159
 // Then, attempt to reassign a new value to PI. 
-let PI = 4.14159
-const pI = 4.141559
+pi = 4.14159
 // Observe the error in the console and write a comment here explaining why you can't reassign a constant.
- 
+ // becouse its constant, we could use let instead.
